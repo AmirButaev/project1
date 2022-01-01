294838e6415f03cb963192ff51ea48580ab0348a
AmirButaev <75184202+AmirButaev@users.noreply.github.com>
2022-01-01 22:23:00 +0300

    проверка внесения изменений

--- webapp/hello_flask.py
+++ webapp/hello_flask.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def do_search() -> "html":
     phrase = request.form["phrase"]
     letters = request.form["letters"]
-    title = "Здесь ваш результат"
+    title = "Здесь ваш результат:"
     results = str(search4letters(phrase, letters))
     return render_template("results.html",
                            the_phrase=phrase,
@@ -22,6 +22,5 @@ def do_search() -> "html":
 def entry_page() -> "html":
     return render_template("entry.html",
                            the_title="Приветствую")
-
 
 app.run(debug=True)
