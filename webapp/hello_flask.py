301157e15dcd1b59eaefaef3b2ccbe65a99567f3
AmirButaev <butaev.bahtiyar@mail.ru>
2022-01-03 11:59:30 +0300

    создание журнала событий

--- webapp/hello_flask.py
+++ webapp/hello_flask.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from vsearch import search4letters
 
 
 app = Flask(__name__)
 
+def log_request(req:"flask_request", res:str) -> None:
+    with open("vsearch.log", "a") as log:
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep="|")
 
 @app.route("/search4", methods=["POST"])
 def do_search() -> "html":
@@ -11,6 +14,7 @@ def do_search() -> "html":
     letters = request.form["letters"]
     title = "Здесь ваш результат:"
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template("results.html",
                            the_phrase=phrase,
                            the_letters=letters,
@@ -23,5 +27,14 @@ def entry_page() -> "html":
     return render_template("entry.html",
                            the_title="Приветствую")
 
-
+@app.route("/viewlog")
+def view_the_log() -> "html":
+    contents = []
+    with open("vsearch.log") as log:
+        for line in log:
+            contents.append([])
+            for item in line.split("|"):
+                contents[-1].append(escape(item))
+    titles = ("Form Data", "Remote_addr", "User_agent", "Result")
+    return render_template("viewlog.html", the_title="View_log", the_row_titles=titles, the_data= contents,)
 app.run(debug=True)
