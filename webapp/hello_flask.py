bdcacfa78430dffcf13e5bc72f2d6d523b787993
AmirButaev <butaev.bahtiyar@mail.ru>
2022-01-07 23:05:23 +0300

    создание UseDatabase

--- webapp/hello_flask.py
+++ webapp/hello_flask.py
@@ -1,30 +1,26 @@
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
-import mysql.connector
+from DBcm import UseDatabase
 
 
 app = Flask(__name__)
 
-def log_request(req:"flask_request", res:str) -> None:
-    dbconfig = { "host": "127.0.0.1",
-                 "user": "root",
-                 "password": "12345",
-                 "database": "vsearchlogDB", }
-    conn = mysql.connector.connect(**dbconfig)
-    cursor = conn.cursor()
+app.config['dbconfig'] = { "host": "127.0.0.1",
+                            "user": "root",
+                            "password": "12345",
+                            "database": "vsearchlogDB", }
 
-    _SQL = """insert into log
-            (phrase, letters, ip, browser_string, results)
-            values
-            (%s, %s, %s, %s, %s)"""
-    cursor.execute(_SQL, (req.form["phrase"],
-                          req.form["letters"],
-                          req.remote_addr,
-                          req.user_agent.browser,
-                          res, ))
-    conn.commit()
-    cursor.close()
-    conn.close()
+def log_request(req:"flask_request", res:str) -> None:
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """insert into log
+                (phrase, letters, ip, browser_string, results)
+                values
+                (%s, %s, %s, %s, %s)"""
+        cursor.execute(_SQL, (req.form["phrase"],
+                              req.form["letters"],
+                              req.remote_addr,
+                              req.user_agent.browser,
+                              res, ))
 
 @app.route("/search4", methods=["POST"])
 def do_search() -> "html":
@@ -47,12 +43,10 @@ def entry_page() -> "html":
 
 @app.route("/viewlog")
 def view_the_log() -> "html":
-    contents = []
-    with open("vsearch.log") as log:
-        for line in log:
-            contents.append([])
-            for item in line.split("|"):
-                contents[-1].append(escape(item))
-    titles = ("Form Data", "Remote_addr", "User_agent", "Result")
-    return render_template("viewlog.html", the_title="View_log", the_row_titles=titles, the_data= contents,)
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """select phrase, letters, ip, browser_string, results from log"""
+        cursor.execute(_SQL)
+        contents = cursor.fetchall()
+        titles = ("Phrase", "Letters", "Remote_addr", "User_agent", "Result")
+        return render_template("viewlog.html", the_title="View_log", the_row_titles=titles, the_data= contents,)
 app.run(debug=True)
